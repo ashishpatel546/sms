9696cb61894bb3d84da80593a9f4cc711b4e9408
simran <simranghanghas555@gmail.com>
2024-02-21 10:53:35 +0530

    env changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
-import { validate } from 'env-validation';
+import { validate } from 'src/env-validation';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DbConfig } from 'src/config/dbconfig';
 import { SharedModule } from './config/shared.module';
