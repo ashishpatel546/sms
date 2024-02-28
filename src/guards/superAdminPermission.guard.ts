2f58d9d42fcd07fa47c76d0c15fcc846a9dc7543
simran <simranghanghas555@gmail.com>
2024-02-28 12:20:58 +0530

    guards-working

---
+++ src/guards/superAdminPermission.guard.ts
@@ -0,0 +1,28 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  Logger,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { USER_ROLE, User } from 'src/users/entity/user.entity';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class SuperAdminGuard implements CanActivate {
+  private logger = new Logger(SuperAdminGuard.name);
+  constructor(private readonly userService: UsersService) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const reqObj: Request = context.switchToHttp().getRequest();
+    const user: Partial<User> = reqObj.user;
+
+    const currentUserData = await this.userService.findUserByEmail(user.email);
+    const currentUser = currentUserData;
+    if (currentUser?.is_active && currentUser.role === USER_ROLE.superadmin) {
+      this.logger.log(`Requested by ADMIN: ${user.email}`);
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
